7055d1c36d0f14128b3651a6a02ac924404d3e02
DongCX-LDHSP <52726994+DongCX-LDHSP@users.noreply.github.com>
2020-08-14 22:12:15 +0800

    重构：将激活引擎的代码块独立出去

--- app/src/main/java/com/practice/facerecognition/WelcomeActivity.java
+++ app/src/main/java/com/practice/facerecognition/WelcomeActivity.java
@@ -1,38 +1,14 @@
 package com.practice.facerecognition;
 
 
-import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.os.Handler;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import com.arcsoft.face.ActiveFileInfo;
-import com.arcsoft.face.ErrorInfo;
-import com.arcsoft.face.FaceEngine;
-import com.arcsoft.face.enums.RuntimeABI;
-import com.practice.facerecognition.common.Constants;
-
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
-
-import io.reactivex.Observable;
-import io.reactivex.ObservableEmitter;
-import io.reactivex.ObservableOnSubscribe;
-import io.reactivex.Observer;
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.schedulers.Schedulers;
 
 public class WelcomeActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -41,25 +17,6 @@ public class WelcomeActivity extends AppCompatActivity implements View.OnClickLi
     Timer timer = new Timer();
     private Handler handler;
     private Runnable runnable;
-
-    // todo 激活引擎代码块 - 1 start
-    // todo 备注：TAG的值 "ChooseFunctionActivity" -> "WelcomeActivity"
-    private static final String TAG = "WelcomeActivity";
-    private static final int ACTION_REQUEST_PERMISSIONS = 0x001;
-    // 在线激活所需的权限
-    private static final String[] NEEDED_PERMISSIONS = new String[]{
-            Manifest.permission.READ_PHONE_STATE
-    };
-    boolean libraryExists = true;
-    // Demo 所需的动态库文件
-    private static final String[] LIBRARIES = new String[]{
-            // 人脸相关
-            "libarcsoft_face_engine.so",
-            "libarcsoft_face.so",
-            // 图像库相关
-            "libarcsoft_image_util.so",
-    };
-    // todo 激活引擎代码块 - 1 end
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -80,11 +37,6 @@ public class WelcomeActivity extends AppCompatActivity implements View.OnClickLi
                 finish();
             }
         }, 5000);//延迟5S后发送handler信息
-
-        // todo 激活引擎代码块 - 2 start
-        libraryExists = checkSoFile(LIBRARIES);
-        activeEngine(null);
-        // todo 激活引擎代码块 - 2 end
     }
 
     private void initView() {
@@ -128,123 +80,4 @@ public class WelcomeActivity extends AppCompatActivity implements View.OnClickLi
                 break;
         }
     }
-
-    // todo 激活引擎代码块 - 3 start
-    /**
-     * 检查能否找到动态链接库，如果找不到，请修改工程配置
-     *
-     * @param libraries 需要的动态链接库
-     * @return 动态库是否存在
-     */
-    private boolean checkSoFile(String[] libraries) {
-        File dir = new File(getApplicationInfo().nativeLibraryDir);
-        File[] files = dir.listFiles();
-        if (files == null || files.length == 0) {
-            return false;
-        }
-        List<String> libraryNameList = new ArrayList<>();
-        for (File file : files) {
-            libraryNameList.add(file.getName());
-        }
-        boolean exists = true;
-        for (String library : libraries) {
-            exists &= libraryNameList.contains(library);
-        }
-        return exists;
-    }
-
-    /**
-     * 权限检查
-     *
-     * @param neededPermissions 需要的权限
-     * @return 是否全部被允许
-     */
-    protected boolean checkPermissions(String[] neededPermissions) {
-        if (neededPermissions == null || neededPermissions.length == 0) {
-            return true;
-        }
-        boolean allGranted = true;
-        for (String neededPermission : neededPermissions) {
-            allGranted &= ContextCompat.checkSelfPermission(this, neededPermission) == PackageManager.PERMISSION_GRANTED;
-        }
-        return allGranted;
-    }
-
-    /**
-     * 激活引擎
-     *
-     * @param view
-     */
-    public void activeEngine(final View view) {
-        if (!libraryExists) {
-            showToast(getString(R.string.library_not_found));
-            return;
-        }
-        if (!checkPermissions(NEEDED_PERMISSIONS)) {
-            ActivityCompat.requestPermissions(this, NEEDED_PERMISSIONS, ACTION_REQUEST_PERMISSIONS);
-            return;
-        }
-        if (view != null) {
-            view.setClickable(false);
-        }
-        Observable.create(new ObservableOnSubscribe<Integer>() {
-            @Override
-            public void subscribe(ObservableEmitter<Integer> emitter) {
-                RuntimeABI runtimeABI = FaceEngine.getRuntimeABI();
-                Log.i(TAG, "subscribe: getRuntimeABI() " + runtimeABI);
-
-                long start = System.currentTimeMillis();
-                int activeCode = FaceEngine.activeOnline(WelcomeActivity.this, Constants.APP_ID, Constants.SDK_KEY);
-                Log.i(TAG, "subscribe cost: " + (System.currentTimeMillis() - start));
-                emitter.onNext(activeCode);
-            }
-        })
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Observer<Integer>() {
-                    @Override
-                    public void onSubscribe(Disposable d) {
-
-                    }
-
-                    @Override
-                    public void onNext(Integer activeCode) {
-                        if (activeCode == ErrorInfo.MOK) {
-                            showToast(getString(R.string.active_success));
-                        } else if (activeCode == ErrorInfo.MERR_ASF_ALREADY_ACTIVATED) {
-                            showToast(getString(R.string.already_activated));
-                        } else {
-                            showToast(getString(R.string.active_failed, activeCode));
-                        }
-
-                        if (view != null) {
-                            view.setClickable(true);
-                        }
-                        ActiveFileInfo activeFileInfo = new ActiveFileInfo();
-                        int res = FaceEngine.getActiveFileInfo(WelcomeActivity.this, activeFileInfo);
-                        if (res == ErrorInfo.MOK) {
-                            Log.i(TAG, activeFileInfo.toString());
-                        }
-
-                    }
-
-                    @Override
-                    public void onError(Throwable e) {
-                        showToast(e.getMessage());
-                        if (view != null) {
-                            view.setClickable(true);
-                        }
-                    }
-
-                    @Override
-                    public void onComplete() {
-
-                    }
-                });
-    }
-
-    protected void showToast(String s) {
-        Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
-    }
-    // todo 激活引擎代码块 - 3 end
 }
